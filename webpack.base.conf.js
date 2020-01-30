3b8129c7be22d881565d7ec733a2412c1ffea1f0
DmitryDarevsky <darevsky.dmitry@gmail.com>
2020-01-30 15:44:34 +0300

    lesson webpack-merge

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -1,18 +1,26 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const PATHS = {
+  src: path.join(__dirname, "./src"),
+  dist: path.join(__dirname, "./dist"),
+  assets: "assets/"
+};
+
 module.exports = {
+  externals: {
+    paths: PATHS
+  },
   entry: {
-    app: "./src/index.js"
+    app: PATHS.src
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "/dist"
+    filename: `${PATHS.assets}js/[name].js`,
+    path: PATHS.dist,
+    publicPath: "/"
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         loader: "babel-loader",
         exclude: "/node_modules/"
@@ -32,7 +40,9 @@ module.exports = {
             loader: "postcss-loader",
             options: {
               sourceMap: true,
-              config: { path: "src/js/postcss.config.js" }
+              config: {
+                path: "src/js/postcss.config.js"
+              }
             }
           }
         ]
@@ -52,7 +62,9 @@ module.exports = {
             loader: "postcss-loader",
             options: {
               sourceMap: true,
-              config: { path: "src/js/postcss.config.js" }
+              config: {
+                path: "src/js/postcss.config.js"
+              }
             }
           },
           {
@@ -65,12 +77,9 @@ module.exports = {
       }
     ]
   },
-  devServer: {
-    overlay: true
-  },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].css"
+      filename: `${PATHS.assets}css/[name].css`
     })
   ]
 };
