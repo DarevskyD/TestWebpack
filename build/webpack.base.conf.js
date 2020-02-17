d771ccac2d1919865d8fb1c9a0bc160186e9f28d
DmitryDarevsky <darevsky.dmitry@gmail.com>
2020-02-17 22:36:24 +0300

    added new config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -11,7 +11,7 @@ const PATHS = {
 };
 
 //const PAGES_DIR = PATHS.src;
-const PAGES_DIR = `${PATHS.src}/html`;
+const PAGES_DIR = PATHS.src;
 const PAGES = fs
   .readdirSync(PAGES_DIR)
   .filter(fileName => fileName.endsWith(".html"));
